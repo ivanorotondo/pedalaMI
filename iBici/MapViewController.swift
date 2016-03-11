535d2ee850b9521e0bf85fcf4fb7a9f19af0ee68
Ivano Rotondo <ivano.rotondo@gmail.com>
2016-03-12 00:45:24 +0100

    mapbox working

--- iBici/MapViewController.swift
+++ iBici/MapViewController.swift
@@ -32,7 +32,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                
                 var marker = MGLPointAnnotation()
-                
+                marker.title = "\((station as? Station)!.stationName)"
+                marker.subtitle = "Biciclette disponibili: \((station as? Station)!.availableBikesNumber)"
 //                marker.icon = UIImage(named: "markerIconMini")
                 marker.coordinate = CLLocationCoordinate2DMake(((station as? Station)?.stationCoord.latitude)!, ((station as? Station)?.stationCoord.longitude)!)
 //                marker.map = self.mapView
@@ -43,6 +44,23 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func mapView(mapView: MGLMapView, imageForAnnotation annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        var annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier("marker")
+        
+        if annotationImage == nil {
+            
+            var image = UIImage(named: "markerIconMini")!
+            annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "marker")
+        }
+        
+        return annotationImage
+    }
+    
+    func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always try to show a callout when an annotation is tapped.
+        return true
     }
 
 //MARK: - getTheHTML call
