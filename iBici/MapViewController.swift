9005bcc1f5ae38862546a248156f4320e4674714
Ivano Rotondo <ivano.rotondo@gmail.com>
2016-03-18 18:45:04 +0100

    graphics and pin set

--- iBici/MapViewController.swift
+++ iBici/MapViewController.swift
@@ -15,43 +15,57 @@ import Mapbox
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 
 //MARK: - vars init
+    let locationManager = CLLocationManager()
+
     let stationsURL = "http://www.bikemi.com/it/mappa-stazioni.aspx"
     var stationsHTML : NSString = ""
     var stationsArray : NSMutableArray = []
     var annotationsArray = [MGLAnnotation]()
     var currentView = ""
+    var startingView = "bikes"
     
 //MARK: outlet init
     @IBOutlet var mapView: MGLMapView!
     @IBOutlet var bikesButton: UIImageView!
-    @IBOutlet var electricBikesButton: UIImageView!
+    @IBOutlet var eBikesButton: UIImageView!
     @IBOutlet var slotsButton: UIImageView!
     @IBOutlet var refreshButton: UIImageView!
     @IBOutlet var settingsButton: UIImageView!
     
-    let locationManager = CLLocationManager()
+//MARK: outlet init TabBar
+    @IBOutlet var bikesItemBackground: UIImageView!
+    @IBOutlet var eBikesItemBackground: UIImageView!
+    @IBOutlet var slotsItemBackground: UIImageView!
 
+    @IBOutlet var eBikesItemView: UIView!
+    @IBOutlet var bikesItemView: UIView!
+    @IBOutlet var slotsItemView: UIView!
+    
 //MARK: - views init
     override func viewDidLoad() {
         
         mapView.styleURL = NSURL(string: "mapbox://styles/ivanorotondo/ciluylzjp00rrc7lu80unjtnr")
         
-        currentView = "bikes"
+        currentView = startingView
+        
+        addTapRecognizerToButtons()
+        updateTabBarItems()
+        downloadAndShowStations()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func addTapRecognizerToButtons(){
         
         let tapBikes = UITapGestureRecognizer(target:self, action:Selector("addBikesMarkers:"))
-        
-        bikesButton.userInteractionEnabled = true
-        bikesButton.addGestureRecognizer(tapBikes)
+        bikesItemView.addGestureRecognizer(tapBikes)
         
         let tapElectricBikes = UITapGestureRecognizer(target:self, action:Selector("addElectricBikesMarkers:"))
-
-        electricBikesButton.userInteractionEnabled = true
-        electricBikesButton.addGestureRecognizer(tapElectricBikes)
-        
+        eBikesItemView.addGestureRecognizer(tapElectricBikes)
+     
         let tapSlots = UITapGestureRecognizer(target: self, action: Selector("addSlotsMarkers:"))
-        
-        slotsButton.userInteractionEnabled = true
-        slotsButton.addGestureRecognizer(tapSlots)
+        slotsItemView.addGestureRecognizer(tapSlots)
         
         let tapRefresh = UITapGestureRecognizer(target: self, action: Selector("refreshMarkers:"))
         
@@ -63,25 +77,143 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         settingsButton.userInteractionEnabled = true
         settingsButton.addGestureRecognizer(tapSettings)
         
+    }
+    
+    func updateTabBarItems(){
+        
+        switch currentView {
+            case "bikes":
+                
+              //set the backgrounds
+                bikesItemBackground.hidden = false
+                eBikesItemBackground.hidden = true
+                slotsItemBackground.hidden = true
+              //set the buttons
+                bikesButton.image = UIImage(named: "bikeSelected.png")
+                eBikesButton.image = UIImage(named: "eBikeUnselected.png")
+                slotsButton.image = UIImage(named: "slotUnselected.png")
+              //set the other buttons
+                refreshButton.image = UIImage(named: "refreshBike.png")
+                settingsButton.image = UIImage(named: "menuBike.png")
+
+            case "electricBikes":
+              //set the background
+                bikesItemBackground.hidden = true
+                eBikesItemBackground.hidden = false
+                slotsItemBackground.hidden = true
+              //set the buttons
+                bikesButton.image = UIImage(named: "bikeUnselected.png")
+                eBikesButton.image = UIImage(named: "eBikeSelected.png")
+                slotsButton.image = UIImage(named: "slotUnselected.png")
+                //set the other buttons
+                refreshButton.image = UIImage(named: "refreshEBike.png")
+                settingsButton.image = UIImage(named: "menuEBike.png")
+            
+            case "slots":
+              //set the background
+                bikesItemBackground.hidden = true
+                eBikesItemBackground.hidden = true
+                slotsItemBackground.hidden = false
+              //set the buttons
+                bikesButton.image = UIImage(named: "bikeUnselected.png")
+                eBikesButton.image = UIImage(named: "eBikeUnselected.png")
+                slotsButton.image = UIImage(named: "slotSelected.png")
+                //set the other buttons
+                refreshButton.image = UIImage(named: "refreshSlot.png")
+                settingsButton.image = UIImage(named: "menuSlot.png")
+            
+            default:
+                //set the backgrounds
+                bikesItemBackground.hidden = false
+                eBikesItemBackground.hidden = true
+                slotsItemBackground.hidden = true
+                //set the buttons
+                bikesButton.image = UIImage(named: "bikeSelected.png")
+                eBikesButton.image = UIImage(named: "eBikeUnselected.png")
+                slotsButton.image = UIImage(named: "slotUnselected.png")
+                //set the other buttons
+                refreshButton.image = UIImage(named: "refreshBike.png")
+                settingsButton.image = UIImage(named: "menuBike.png")
+        }
+        
+    }
+    
+    func downloadAndShowStations(){
+        
         getTheHTML(stationsURL){
             ParsingSDK.parseGoogleMapToObjects(self.stationsHTML, stationsArray: self.stationsArray)
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 
-                self.addMarkersToTheMap("bikes")
+                self.addMarkersToTheMap(self.currentView)
             })
         }
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
     }
     
     func mapView(mapView: MGLMapView, imageForAnnotation annotation: MGLAnnotation) -> MGLAnnotationImage? {
-        var annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier("marker")
+
         
-        if annotationImage == nil {
+      //      var annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(currentView)
+            var image = UIImage(named: "pinBike")!
+
+            switch currentView {
+                case "bikes":
+       //             annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(currentView)
+        //            if annotationImage == nil {
+                        image = UIImage(named: "pinBike")!
+        //            }
+                
+                case "electricBikes":
+         //           annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(currentView)
+       //             if annotationImage == nil {
+                    image = UIImage(named: "pinEBike")!
+         //       }
+                case "slots":
+         //           annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(currentView)
+         //           if annotationImage == nil {
+                    image = UIImage(named: "pinSlot")!
+         //       }
+                default:
+        //            annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier(currentView)
+         //           if annotationImage == nil {
+                    image = UIImage(named: "pinBike")!
+        //            }
+            }
+        
+            var text = NSString(string: " ")
+
+            if annotation.subtitle != nil{
+                var subtitle = (annotation.subtitle! as! String?)
+                let numberIndex = (subtitle?.startIndex.distanceTo((subtitle?.characters.indexOf(":"))!))! + 2
+                let subtitleNSString = NSString(string: "\(subtitle!)")
+                text = subtitleNSString.substringWithRange(NSRange(location: numberIndex, length: (subtitle?.characters.count)! - numberIndex))
+            }
+        
+            let textColor: UIColor = UIColor.whiteColor()
+            let textFont: UIFont = UIFont(name: "Helvetica Bold", size: 15)!
+            let textFontAttributes = [
+                NSFontAttributeName: textFont,
+                NSForegroundColorAttributeName: textColor,
+            ]
+        
+            let size = CGSize(width: 25, height: 25)
+            UIGraphicsBeginImageContext(size)
+            image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        
+            var rect: CGRect = CGRectMake(8.5, 4, size.width, size.width)
+
+        print("\(text)")
+            if text.length == 1 {
+                rect = CGRectMake(8.5, 4, size.width, size.width)
+            }
+            if text.length == 2 {
+                rect = CGRectMake(4.5, 4, size.width, size.width)
+            }
+        
+            text.drawInRect(rect, withAttributes: textFontAttributes)
+            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
             
-            var image = UIImage(named: "markerIconMini")!
-            annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "marker")
-        }
+            var annotationImage = MGLAnnotationImage(image: resizedImage, reuseIdentifier: "\(currentView) \(text)")
         
         return annotationImage
     }
@@ -154,28 +286,33 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 //    }
     
     
-    func addElectricBikesMarkers(sender: UITapGestureRecognizer) {
-        if currentView != "electricBikes" {
-            mapView.removeAnnotations(annotationsArray)
-            addMarkersToTheMap("electricBikes")
-            currentView = "electricBikes"
-        }
-    }
+    
     
     func addBikesMarkers(sender: UITapGestureRecognizer) {
         if currentView != "bikes" {
             mapView.removeAnnotations(annotationsArray)
-            addMarkersToTheMap("bikes")
             currentView = "bikes"
+            updateTabBarItems()
+            addMarkersToTheMap("bikes")
         }
 
+    }
+    
+    func addElectricBikesMarkers(sender: UITapGestureRecognizer) {
+        if currentView != "electricBikes" {
+            mapView.removeAnnotations(annotationsArray)
+            currentView = "electricBikes"
+            updateTabBarItems()
+            addMarkersToTheMap("electricBikes")
+        }
     }
     
     func addSlotsMarkers(sender: UITapGestureRecognizer) {
         if currentView != "slots" {
             mapView.removeAnnotations(annotationsArray)
-            addMarkersToTheMap("slots")
             currentView = "slots"
+            updateTabBarItems()
+            addMarkersToTheMap("slots")
         }
     }
     
