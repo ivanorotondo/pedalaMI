f67efe396506f18e933004d604e561f23d53c91f
Ivano Rotondo <ivano.rotondo@gmail.com>
2016-04-22 12:21:20 +0200

    new graphics

--- Pods/Target Support Files/Pods-pedalaMI/Pods-pedalaMI-frameworks.sh
+++ Pods/Target Support Files/Pods-pedalaMI/Pods-pedalaMI-frameworks.sh
@@ -86,10 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
   install_framework "Pods-pedalaMI/Rollbar.framework"
-  install_framework "Pods-pedalaMI/SlideMenuControllerSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
   install_framework "Pods-pedalaMI/Rollbar.framework"
-  install_framework "Pods-pedalaMI/SlideMenuControllerSwift.framework"
 fi
